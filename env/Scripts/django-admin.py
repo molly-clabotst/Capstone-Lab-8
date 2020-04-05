abd1c634ba7d95c041815a6caa2cefa26760399b
Molly Clabots <molly.clabots@gmail.com>
2020-04-05 00:52:35 -0500

    Adding error handling for if a user tries to say they have visited a location that has not been entered yet

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\molly clabots\onedrive - mnscu\documents\itec 2905\lab8\django_wishlist\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
