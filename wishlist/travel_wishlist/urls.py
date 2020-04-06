2b2f3ac888f2a5a2707cc50d3154b53256265486
Molly Clabots <molly.clabots@gmail.com>
2020-04-05 23:38:55 -0500

    Changing styling, adding comments

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.place_list, name='place_list'),
     path('visited', views.places_visited, name='places_visited'),
+    # <int: var_name> allows for the insertion of a variable
     path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited')
     
 ]
