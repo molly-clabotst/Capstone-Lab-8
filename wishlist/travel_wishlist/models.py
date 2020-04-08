2dc3623ba7c2f8a429576f8ff84ac19fb0010040
Molly Clabots <molly.clabots@gmail.com>
2020-04-07 21:41:00 -0500

    Updating model

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=False)
+    photo = models.ImageField(upload_to='use_images/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}, visited? {self.visited}'
+        return f'{self.name}, visited? {self.visited} on {self.date_visited}\nPhoto {photo_str}'
