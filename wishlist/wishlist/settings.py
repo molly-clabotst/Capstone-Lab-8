c2dbc63db4783130cd1f70582cf4090f200c3082
Molly Clabots <molly.clabots@gmail.com>
2020-04-23 16:19:16 -0500

    Updating settings to use postgress database

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'places',
+        'USER': 'traveler',
+        'PASSWORD': os.getenv('TRAVELER_PW'),
+        'HOST': '/cloudsql/wishlist-275022:us-central1:wishlist-db',
+        'PORT': '5432'
     }
 }
 
